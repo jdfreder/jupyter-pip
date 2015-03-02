c4aac08e2a0ca078e346fb043f9ee0c778f05d2c
Jonathan Frederic <jon.freder@gmail.com>
2015-03-02 11:41:47 -0800

    Change PYPI from ipython-pip to jupyter-pip.

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 from setuptools import setup
 
 setup(
-    name='ipython-pip',
-    version='0.3',
+    name='jupyter-pip',
+    version='0.1',
     description='Allows IPython notebook extension authors to make their extension pip installable!',
     author='Jonathan Frederic',
     author_email='jon.freder@gmail.com',
     license='New BSD License',
-    url='https://github.com/jdfreder/ipython-pip',
-    keywords='python ipython javascript nbextension deployment pip install package extension',
+    url='https://github.com/jdfreder/jupyter-pip',
+    keywords='python jupyter ipython javascript nbextension deployment pip install package extension',
     classifiers=['Development Status :: 4 - Beta',
                  'Programming Language :: Python',
                  'License :: OSI Approved'],
-    packages=['ipythonpip'],
+    packages=['jupyterpip'],
     include_package_data=True,
 )
